80543697e6080867176eb94f20b75a670b64e271
Anand Singh <43192082+the-anand-singh@users.noreply.github.com>
2021-04-19 22:11:01 +0530

    version 0.5
    Added time functionality - the time is ticking!

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from kivy.app import App
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
+from kivy.clock import Clock
+import time
+from datetime import datetime
 
 
 class ClockApp(App):
-	pass
+	def update_time(self, nap):
+		self.root.ids.time.text = datetime.now().strftime('[b]%H[/b]:%M:%S')
+
+	def on_start(self):
+		Clock.schedule_interval(self.update_time, 1)
 
 
 if __name__ == '__main__':
