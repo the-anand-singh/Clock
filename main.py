ca40fb943fa8aebe82426c0b3d89335e60a7feb8
Anand Singh <43192082+the-anand-singh@users.noreply.github.com>
2021-04-19 21:57:36 +0530

    version 0.4
    Updated Background color

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # File: main.py
 from kivy.app import App
 from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
 
 
 class ClockApp(App):
@@ -8,6 +10,7 @@ class ClockApp(App):
 
 
 if __name__ == '__main__':
+	Window.clearcolor = get_color_from_hex('#38548a')
 	LabelBase.register(name='Roboto',
 		fn_regular='Roboto-Light.ttf',
 		fn_bold='Roboto-Medium.ttf')
